3516e08396fafc5a1e1343ed8b60adffedff72e7
Henish Patel <200120107182@git.org.in>
2022-05-13 23:27:29 +0530

    Message app models and admin page created.

--- message/admin.py
+++ message/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(ReceivedMessages)
+class ReceivedMessagesAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "date_created", "response_sent"]
+    # readonly_fields = ["name", "email", "phone_number", "message", "ip_address"]
+    
+    def response_sent(self, msg):
+        if len(msg.response) > 0:
+            return "Yes"
+        return "No"
+    
+
+@admin.register(NewsLetterSubscriber)
+class NewsLetterSubscriberAdmin(admin.ModelAdmin):
+    list_display = ["email", "date_created", "mail_send"]
+    # readonly_fields = ['email']
+    
+    def mail_send(self, sub):
+        number = SendMail.objects.filter(date_created__gte=sub.date_created).count()
+        return number
+        
+
+@admin.register(SendMail)
+class SendMailAdmin(admin.ModelAdmin):
+    list_display = ['id', 'subject', 'date_created']
+        
--- message/models.py
+++ message/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from ckeditor import fields
 
 # Create your models here.
+
+
+class ReceivedMessages(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    message = models.TextField()
+    ip_address = models.CharField(max_length=150)    
+    response = fields.RichTextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return str(self.id) + self.name
+    
+    class Meta:
+        ordering = ["response"]
+    
+    
+class NewsLetterSubscriber(models.Model):
+    email = models.EmailField(unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.email
+    
+
+class SendMail(models.Model):
+    subject = models.CharField(max_length=200, blank=True, null=True)
+    body = fields.RichTextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + self.subject 
+    
+    
+    
