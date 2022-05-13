4e18c1852f17e935b240f2712948a99194287213
Henish Patel <200120107182@git.org.in>
2022-05-13 16:16:35 +0530

    First Commit.

---
+++ core/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from ckeditor import fields
+
+# Create your models here.
+
+class SiteInfo(models.Model):
+    businessName = models.CharField(max_length=100, default="Mj Enterprise")
+    logo = models.ImageField(upload_to="site/")
+    aboutUs = fields.RichTextField()
+    whatsapp = models.URLField()
+    facebook = models.URLField(null=True, blank=True)
+    instagram = models.URLField(null=True, blank=True)
+    gmail = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    reg_no = models.CharField(max_length=100)
+    gst_in_no = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.businessName
+
+    class Meta:
+        ordering = ['-last_updated']
+        
+
+class AboutImage(models.Model):
+    image = models.ImageField(upload_to="about/")
+    subtitle = models.CharField(max_length=100, blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        if self.subtitle:
+            return self.subtitle
+        return super().__str__()
+    
+    class Meta:
+        ordering = ['-date_created']
+    
+    
